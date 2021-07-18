d75482c5d9f52ea06bbea6ab34b95811317b7efb
Nathan Dai <61402904+NathanDai5287@users.noreply.github.com>
2021-07-17 21:31:28 -0700

    remove secret key

--- schedule/settings.py
+++ schedule/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=k_xx3yn$tcm8whxnr38)lt=tgw4fqs7keryq4%^5c&x3*(w3q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
