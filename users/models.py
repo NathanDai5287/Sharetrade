e05ff0c19081891fe961b26cd19292427ba4750b
Liang Dai <dailiang2000@gmail.com>
2021-08-01 21:55:18 -0700

    implemented SQL

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL  import Image
 
-class Schedule(models.Model):
-	start_time = models.DateTimeField()
-	end_time = models.DateTimeField()
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+# class Schedule(models.Model):
+# 	start_time = models.DateTimeField()
+# 	end_time = models.DateTimeField()
+# 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,6 +14,18 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
+
+	def save(self):
+		super().save()
+		img = Image.open(self.image.path)
+		if img.height > img.width:
+			img = img.crop((0, (img.height-img.width)/2, img.width, (img.height+img.width)/2))
+		if img.height < img.width:
+			img = img.crop(((img.width-img.height)/2, 0, (img.width+img.height)/2, img.height))
+		if img.height > 300:
+			output_size = (300, 300)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
 
 class Stock(models.Model):
 	symbol = models.CharField(max_length=4)
--- users/views.py
+++ users/views.py
@@ -3,20 +3,32 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Stock
 
 @login_required
 def home(request):
 	if request.method == 'GET':
-		return render(request, "users/edit.html")
+		portfolio = Stock.objects.filter(user=request.user)
+		return render(request, "users/edit.html", {'portfolio': portfolio})
 	else:
 		portfolio = dict()
 		for key, value in request.POST.items():
 			if 'ticker' in key:
 				ticker = value
 			elif 'quantity' in key:
-				portfolio[ticker] = int(value)
+				if ticker:
+					portfolio[ticker] = int(value)
+					stock = Stock.objects.filter(symbol=ticker, user=request.user).first()
+					if stock:
+						if stock.quantity != portfolio[ticker]:
+							stock.quantity = portfolio[ticker]
+							stock.save()
+					else:
+						stock = Stock(symbol=ticker, quantity=portfolio[ticker], user=request.user)
+						stock.save()
 		return render(request, "users/analysis.html", {'portfolio': portfolio})
 
 def register(request):
@@ -33,4 +45,20 @@ def register(request):
 
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			messages.success(request, f'Your account, has been updatrd!')
+			return redirect('profile')
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)
+
+	context = {
+		'u_form': u_form,
+		'p_form': p_form
+	}
+	return render(request, 'users/profile.html', context)
